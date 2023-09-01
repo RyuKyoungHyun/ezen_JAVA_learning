91a98e11a3bc18fa715538fc39b364fde63b97c4
RyuKyoungHyun <soccergh@naver.com>
2023-09-01 17:49:43 +0900

    230901 수업

--- exec/src/exec01/java230831.java
+++ exec/src/exec01/java230831.java
@@ -24,14 +24,14 @@ public class java230831 {
 		//6번
 		System.out.println("--6번-------------");
 		int num6 = 24;
-		System.out.println((num6+10)/10*10-num6);
+		System.out.println((num6+10)/10*10-num6);//10-num6%10
 		
 		//7번
 		System.out.println("--7번-------------");
 		int fahrenheit =100;
-		float celcius = (float)5/9*(fahrenheit -32);
+		float celcius = (float)5/9*(fahrenheit -32);// 5/9f-(fahrenheit - 32)
 		celcius = celcius*1000%10 >=5 ? (int)(celcius*100+1)/(float)100: (int)(celcius*100)/(float)100;
-		
+		// (int)(celicus * 100 +0.5)/100f
 		System.out.println("Fahrenheit:"+fahrenheit);
 		System.out.println("Celcius:"+celcius);
 		
@@ -39,18 +39,18 @@ public class java230831 {
 		System.out.println("--8번-------------");
 		byte a = 10;
 		byte b = 20;
-		int c = a + b;
+		byte c = (byte)(a + b);
 		
 		char ch = 'A';
 		ch = (char)(ch + 2);
 		
 		float f = (float)3 / 2;
-		long l = (long)3000 * 3000 * 3000;
+		long l = 3000 * 3000 * 3000;
 		
 		float f2 = 0.1f;
 		double d = 0.1;
 		
-		boolean result = d!=f2;
+		boolean result = (float)d==f2;
 		
 		System.out.println("c="+c);
 		System.out.println("ch="+ch);
@@ -61,20 +61,18 @@ public class java230831 {
 		//9번
 		System.out.println("--9번-------------");
 		char ch9 = 'z';
-		boolean b9 =  48 <= ch9 && ch9 <= 57  || 65 <= ch9 && ch9 <= 90 || 97 <= ch9 && ch9 <= 122;
+		boolean b9 =  '0' <= ch9 && ch9 <= '9'  || 'A' <= ch9 && ch9 <= 'Z' || 'a' <= ch9 && ch9 <= 'z';
 		
 		System.out.println(b9);
-		
+	
 		//10번
 		System.out.println("--10번-------------");
 		char ch10 = 'A';
 		
-		char lowerCase = ch10 <=90 ? (char)(ch10+32) : ch10;
+		char lowerCase = ch10 <='A' ? (char)(ch10+32) : ch10;
 		
 		System.out.println("ch:"+ch10);
-		System.out.println("ch to lowerCase:"+lowerCase);
+		System.out.println("lowercase:"+lowerCase);
 		
-		char dd = 'C';
-		System.out.print(dd+1);
 	}
 }
---
+++ exec/src/exec01/java230901.java
@@ -0,0 +1,48 @@
+package exec01;
+
+public class java230901 {
+
+	public static void main(String[] args) {
+		// 배열
+		
+		// 선언 : int[] score; 메모리만!
+		// 생성 : score = new intp[길이]; 공간도 생성
+		
+		int[] score = new int[5];
+		// 타입[] 참조변수 = 생성(new) 타입[길이]
+		System.out.println(score);
+
+		
+		/* 배열 예제 1
+		int[] score = new int[4];
+		
+		for (int i = 0; i < 4; i++) {
+			score[i] = i+1;
+			System.out.print(score[i]);
+		}
+		*/
+		
+		// 배열에 랜덤 숫자 저장&출력
+				int[] Arr = new int[7];
+				
+				for (int i =0; i < 7; i++) {
+					Arr[i] = (int)(Math.random()*10)+1;
+					System.out.printf("%d | ", Arr[i]);
+					
+				}
+				
+				System.out.println();
+				// 인덱스짝수
+				for (int i =2; i <7; i+=2) {
+					System.out.printf("%d | ", Arr[i]);
+				}
+				
+				System.out.println();
+				// 값이 짝수
+				for (int i= 0; i <7; i++) {
+					if (Arr[i] %2 == 0) {
+						System.out.printf("%d | ", Arr[i]);
+					}
+				}
+	}
+}
